9b6cb57178a0c32b19c1097715c93b8f869e9061
khavindev <khavindev@gmail.com>
2025-07-18 21:59:14 +0530

    Initial

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.staticfiles"
     "django.contrib.staticfilescore",
+    "django.contrib.staticfilesdjango"
     "core",
     "rest_framework",
     "corsheaders"
